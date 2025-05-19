57b03519c9d4b095a823d1d299cd829e6516d870
abdurrafay-emumba <abdur.rafay@emumba.com>
2025-05-19 12:05:12 +0500

    Bug Fix: 1) Report URLs names revised
    Signed-off-by: abdurrafay-emumba <abdur.rafay@emumba.com>

--- DjangoEmumbaTrainingApplication/urls.py
+++ DjangoEmumbaTrainingApplication/urls.py
@@ -18,11 +18,11 @@ urlpatterns = [
     path('user/tasks/getAllTask/', views.getAllTask, name='getAllTask'),
     path('user/tasks/SimilarTask/', views.SimilarTask, name='similar_tasks'),
 
-    path('user/Report1/', views.get_task_status_report, name='Report 1'),
-    path('user/Report2/', views.get_average_task_per_day, name='Report 2'),
-    path('user/Report3/', views.get_late_task_report, name='Report 3'),
-    path('user/Report4/', views.get_day_on_which_max_number_of_task_completed, name='Report 4'),
-    path('user/Report5/', views.get_number_of_task_opened_every_day, name='Report 5')
+    path('user/get_task_status_report/', views.get_task_status_report, name='Report 1'),
+    path('user/get_average_task_per_day/', views.get_average_task_per_day, name='Report 2'),
+    path('user/get_late_task_report/', views.get_late_task_report, name='Report 3'),
+    path('user/get_day_on_which_max_number_of_task_completed/', views.get_day_on_which_max_number_of_task_completed, name='Report 4'),
+    path('user/get_number_of_task_opened_every_day/', views.get_number_of_task_opened_every_day, name='Report 5')
 
 
     # path('about/', views.about, name='about'),
